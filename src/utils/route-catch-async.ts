15ad071b679652ecb7cfd433a24521c71516ee66
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-07 17:31:42 -0400

    improved route-catch-async

--- src/utils/route-catch-async.ts
+++ src/utils/route-catch-async.ts
@@ -1,14 +1,22 @@
 import { NextFunction, Request, Response } from 'express';
 
-// Types
-type AsyncRouteHandler<T> = (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => Promise<T>;
-
-export const routeCatchAsync = <T>(asyncRouteHandler: AsyncRouteHandler<T>) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    asyncRouteHandler(req, res, next).catch(next);
+export const routeCatchAsync = <
+  TParams = unknown,
+  TResBody = unknown,
+  TReqBody = unknown,
+  TReqQuery = unknown,
+>(
+  handler: (
+    req: Request<TParams, TResBody, TReqBody, TReqQuery>,
+    res: Response,
+    next: NextFunction,
+  ) => Promise<void>,
+) => {
+  return (
+    req: Request<TParams, TResBody, TReqBody, TReqQuery>,
+    res: Response,
+    next: NextFunction,
+  ) => {
+    handler(req, res, next).catch(next);
   };
 };
