3ed5906f53027b3c018c02b2c4ea2e36e0a3a563
Mario Arancibia Jimenez <mario.arancibia.jimenez@gmail.com>
2025-04-09 15:50:40 -0400

    minor change to handle-route-error.test

--- src/middleware/handle-route-error.test.ts
+++ src/middleware/handle-route-error.test.ts
@@ -1,10 +1,7 @@
+import { HTTP_STATUS } from '@/constants/http-status';
 import { HttpError } from '@/utils/http-error';
 import { NextFunction, Request, Response } from 'express';
 import { handleRouteError } from './handle-route-error';
-
-// Constants
-const STATUS_BAD_REQUEST = 400;
-const STATUS_SERVER_ERROR = 500;
 
 describe('handleRouteError', () => {
   let req: Partial<Request>;
@@ -18,14 +15,14 @@ describe('handleRouteError', () => {
   });
 
   it('should return specific status and message', () => {
-    const error = new HttpError(STATUS_BAD_REQUEST, 'Test error');
+    const error = new HttpError(HTTP_STATUS.NOT_FOUND, 'Test error');
 
     handleRouteError(error, req as Request, res as Response, next);
 
-    expect(res.status).toHaveBeenCalledWith(STATUS_BAD_REQUEST);
+    expect(res.status).toHaveBeenCalledWith(HTTP_STATUS.NOT_FOUND);
     expect(res.json).toHaveBeenCalledWith(
       expect.objectContaining({
-        status: STATUS_BAD_REQUEST,
+        status: HTTP_STATUS.NOT_FOUND,
         message: 'Test error',
       }),
     );
@@ -36,10 +33,10 @@ describe('handleRouteError', () => {
 
     handleRouteError(error as HttpError, req as Request, res as Response, next);
 
-    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.status).toHaveBeenCalledWith(HTTP_STATUS.SERVER_ERROR);
     expect(res.json).toHaveBeenCalledWith(
       expect.objectContaining({
-        status: STATUS_SERVER_ERROR,
+        status: HTTP_STATUS.SERVER_ERROR,
         message: 'Internal Server Error',
       }),
     );
